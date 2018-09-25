983ea8271a0a1d3d0efbfbf2cace48a373893673
Zippen-Huang <1508303949@qq.com>
2018-09-25 13:42:46 +0800

    Add files via upload
    read PPm signal from tele-reveiver by MCU IO

---
+++ ppmread.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+Servo servoread1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  servoread1.attach(2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ppm1 = servoread1.readMicroseconds();
+  Serial.println(ppm1);
+  
+}
